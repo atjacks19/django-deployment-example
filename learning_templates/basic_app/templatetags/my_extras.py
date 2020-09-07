965bb8aadf88fba069c1351067c838c447bd334a
Aaron Jackson <atjacksdeveloper@gmail.com>
2020-09-07 15:58:30 +0100

    fixed template tag

--- learning_templates/basic_app/templatetags/my_extras.py
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -2,7 +2,7 @@ from django import template
 
 register = template.Library()
 
-@register.filter('cutting', cutting)
+@register.filter(name='cutting')
 def cutting(value,arg):
     """
     This cuts out all values of "arg" from the string!
